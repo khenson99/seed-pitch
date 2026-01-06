44b89e2135f473f1304e816df3b04ee70c8ec8a0
Kyle <kyle@arda.cards>
2026-01-06 13:44:38 -0800

    Change ClosingSlide animation to fade out

--- src/components/slides/ClosingSlide.tsx
+++ src/components/slides/ClosingSlide.tsx
@@ -22,7 +22,7 @@ export const ClosingSlide: React.FC = () => {
       }}>
         <motion.div 
           initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
+          whileInView={{ opacity: 0, y: 0 }}
           transition={{ duration: 0.8 }}
           style={{ textAlign: 'center' }}
         >
